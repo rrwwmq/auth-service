5fee805336caf31dc7057578a9ebfd490a2e2636
Altan <altan@MacBook-Pro-Altan.local>
2026-03-31 20:58:51 +0300

    change min len password

--- internal/transport/rest/dto.go
+++ internal/transport/rest/dto.go
@@ -2,14 +2,13 @@ package rest
 
 type RegisterRequest struct {
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6"`
+	Password string `json:"password" binding:"required,min=8"`
 }
 
 type LoginRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
 }
-
 type ErrorResponse struct {
 	Error string `json:"error"`
 }
